8cfa2e4aca27f1770150bb20d913f9c48839dde0
Nicholas Jativa <nicholas.jativa@outlook.com>
2018-12-10 19:32:15 -0500

    Fixed crossDomain issue

--- dist/config/allowCrossDomain.js
+++ dist/config/allowCrossDomain.js
@@ -4,14 +4,16 @@ function allowCrossDomain(req, res, next) {
     const allowedOrigins = [
         "localhost:8999", "localhost:4200",
         "http://localhost:4200", "http://localhost:8999",
-        "API_URL"
+        "https://whispering-woodland-59077.herokuapp.com",
+        "http://whispering-woodland-59077.herokuapp.com",
+        "whispering-woodland-59077.herokuapp.com"
     ];
     let incomingOrigin;
     try {
         incomingOrigin = allowedOrigins.indexOf(req.headers.host.toLowerCase()) > -1 ? req.headers.origin.toString() : "API_URL";
     }
     catch (error) {
-        incomingOrigin = "API_URL";
+        incomingOrigin = "whispering-woodland-59077.herokuapp.com";
     }
     res.header("Access-Control-Allow-Origin", incomingOrigin);
     res.header("Access-Control-Allow-Credentials", "true");
