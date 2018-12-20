3dfdf3b0eb48adc375cb80535d3bbbf04d09b2c6
Nicholas Jativa <nicholas.jativa@outlook.com>
2018-12-20 17:02:19 -0500

    Cleaned out git cache to ignore dist files

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-const app = new server_1.AmadeusServer().getApp();
-exports.app = app;
-//# sourceMappingURL=index.js.map
