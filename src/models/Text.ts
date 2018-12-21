8e3c0aa8d9859ae28b2e9ef1462f2ae710713c7a
Nicholas Jativa <nicholas.jativa@outlook.com>
2018-12-20 21:18:50 -0500

    Refactored Text model

--- src/models/Text.ts
+++ src/models/Text.ts
@@ -1,67 +1,55 @@
 import * as db from "../db.js";
+import { MysqlError } from "mysql";
 
 export class Text {
 
+    constructor() {
+    }
     // TODO this will have to be refactored since we dont know the thread id when a
     // message from a new phone comes in
-    static create(msgid_phone_db, phone_num_clean, fromPhoneNumber, toPhoneNumber, textMessageBody, timestamp, cb): void {
+    public static create(msgid_phone_db, phone_num_clean, fromPhoneNumber, toPhoneNumber, textMessageBody, timestamp, cb: (err: MysqlError, result: any) => void): void {
+        
         const values = [fromPhoneNumber, toPhoneNumber, textMessageBody, timestamp, timestamp + toPhoneNumber,
                         msgid_phone_db, phone_num_clean];
+        const query = `INSERT INTO texts
+                        (fromPhoneNumber, toPhoneNumber, textMessageBody, timestamp, 
+                        amadeusId, msgid_phone_db, phone_num_clean)
+                        VALUES (?, ?, ?, ?, ?, ?, ?)`;
 
-        db.get().query(`INSERT INTO texts
-                        (fromPhoneNumber, toPhoneNumber, textMessageBody, timestamp, amadeusId, msgid_phone_db, phone_num_clean)
-                        VALUES (?, ?, ?, ?, ?, ?, ?)`,
-                        values,
-                        (err, result) => {
-                            if (err) return cb(err, undefined);
-                            else {
-                                return cb(undefined, result);
-                            }
-                        });
+        db.get().query(query, values, (err: MysqlError, result: any) => cb(err, result));
     }
 
-    static getAllMessages(phone_num_clean: number, userId: number, cb): void {
-        db.get().query(`SELECT *
-                        FROM texts
-                        WHERE phone_num_clean = ?
-                        AND userId = ?
-                        ORDER BY timestamp
-                        DESC LIMIT 30`,
-                        [phone_num_clean, userId],
-                        (err, rows) => {
-                            if (err) return cb(err);
-                            cb(undefined, rows.reverse());
-            });
+    public static getAllMessages(phone_num_clean: number, userId: number, cb: (err: MysqlError, rows: any) => void): void {
+
+        const query: string = `SELECT *
+                                FROM texts
+                                WHERE phone_num_clean = ?
+                                AND userId = ?
+                                ORDER BY timestamp
+                                DESC LIMIT 30`;
+        const values = [phone_num_clean, userId];
+        
+        db.get().query(query, values, (err: MysqlError, rows: any) => cb(err, rows.reverse()));
     }
 
-    static updateMesssageStatus(id, status: string, cb?): void {
+    public static updateMesssageStatus(id: number, status: string, cb: (err: MysqlError, result: any) => void): void {
 
+        const query: string = `UPDATE texts
+                                SET status = ?
+                                WHERE amadeusId = ?`; 
         const values = [status, id];
 
-        db.get().query(`UPDATE texts
-                        SET status = ?
-                        WHERE amadeusId = ?`,
-                        values,
-                        (err, result) => {
-                            if (typeof cb === "function" && cb()) {
-                                if (err) return cb(err, undefined);
-                                if (result) return cb(undefined, result);
-                            }
-                        });
+        db.get().query(query, values, (err: MysqlError, result: any) => cb(err, result));
     }
 
-    static updateMessageId(amadeusId, msgid_phone_db, cb?): void {
+    public static updateMessageId(amadeusId, msgid_phone_db, cb: (err: MysqlError, result: any) => void): void {
 
+        const query: string = `UPDATE texts
+                                SET msgid_phone_db = ?
+                                WHERE amadeusId = ?`;
         const values = [msgid_phone_db, amadeusId];
 
-        db.get().query(`UPDATE texts
-                        SET msgid_phone_db = ?
-                        WHERE amadeusId = ?`,
-                        values,
-                        (err, results) => {
-                            if (err) return cb(err, undefined);
-                            return cb(undefined, results);
-                        });
+        db.get().query(query, values, (err: MysqlError, results: any) => cb(err, results));
     }
 
 }
